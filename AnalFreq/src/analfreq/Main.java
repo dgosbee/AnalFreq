2b0f5518e4d2ba81af81f3dfe384bd1dee68110d
hommel <>
2015-04-25 08:45:57 -0400

    Basic cleanup. Shortened code where possible.

--- AnalFreq/src/analfreq/Main.java
+++ AnalFreq/src/analfreq/Main.java
@@ -1,7 +1,6 @@
 package analfreq;
 
 import analfreq.config.Config;
-import analfreq.freqevent.FreqEvent;
 import analfreq.gui.DisplayChart;
 import analfreq.gui.UIControls;
 import javafx.application.Application;
@@ -9,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.chart.BubbleChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.image.Image;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
@@ -17,18 +15,22 @@ public class Main extends Application {
 
     private static final BubbleChart bubbleChart = DisplayChart.createDisplayChart();
 
+    /**
+     * Plots a new object onto the bubble chart. This method is called 
+     * after the user clicks the submit button when adding a new instrument.
+     * 
+     * @param series 
+     */
     public static void plotObject(XYChart.Series series) {
         bubbleChart.getData().addAll(series);
     }
 
     @Override
     public void start(Stage stage) {
-        FlowPane uiControls = UIControls.createUIControls();
         stage.setTitle(Config.stageTitle);
         HBox root = new HBox();
-        root.getChildren().addAll(bubbleChart, uiControls);
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        root.getChildren().addAll(bubbleChart, UIControls.createUIControls());
+        stage.setScene(new Scene(root));
         stage.getIcons().add(new Image("icon/Asshole.jpg"));
         stage.show();
     }
